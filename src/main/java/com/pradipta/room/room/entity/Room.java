445141489ea54681b60e415f1af75b8e45843d8d
Pradipta Sarma <pradipta.sarma@UE-B-NOTE-0046.local>
2020-02-12 11:04:13 +0530

    room entity

---
+++ src/main/java/com/pradipta/room/room/entity/Room.java
@@ -0,0 +1,26 @@
+package com.pradipta.room.room.entity;
+
+import com.pradipta.room.user.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Room {
+    @Id
+    private int id;
+    private String roomName;
+    private String roomDescription;
+    @OneToMany(targetEntity = User.class, cascade = CascadeType.ALL)
+    @JoinColumn(name = "room_user_fk", referencedColumnName = id)
+    private List<User> users;
+    private Boolean isActive = true;
+}
