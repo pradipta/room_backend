a6849054a0c932a5d0da78e795f93c7367f05bb8
Pradipta Sarma <pradipta.sarma@UE-B-NOTE-0046.local>
2020-02-12 11:23:23 +0530

    small fix

--- src/main/java/com/pradipta/room/room/entity/Room.java
+++ src/main/java/com/pradipta/room/room/entity/Room.java
@@ -20,7 +20,7 @@ public class Room {
     private String roomName;
     private String roomDescription;
     @OneToMany(targetEntity = User.class, cascade = CascadeType.ALL)
-    @JoinColumn(name = "room_user_fk", referencedColumnName = id)
+    @JoinColumn(name = "room_user_fk", referencedColumnName = "username")
     private List<User> users;
     private Boolean isActive = true;
 }
